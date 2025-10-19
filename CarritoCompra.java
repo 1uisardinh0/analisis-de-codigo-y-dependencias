ffcb625bf1940de5a920607ca8f9d5e6e5e7ed0d
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-10-18 22:51:57 -0300

    Se incorporan los archivos generados por visual paradigm

---
+++ CarritoCompra.java
@@ -0,0 +1,30 @@
+public class CarritoCompra {
+
+	private int produtos;
+
+	public CarritoCompra() {
+		// TODO - implement CarritoCompra.CarritoCompra
+		throw new UnsupportedOperationException();
+	}
+
+	public int calcularTotal() {
+		// TODO - implement CarritoCompra.calcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cant
+	 * @param precio
+	 */
+	public int subTotal(int cant, int precio) {
+		// TODO - implement CarritoCompra.subTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		// TODO - implement CarritoCompra.mostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
